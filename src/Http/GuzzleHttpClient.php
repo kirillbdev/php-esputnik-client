d3313c68b1da1efd7f5bb3936ff7f693539c0bcf
kirillbdev <kirill.babinec@gmail.com>
2019-11-18 16:36:51 +0200

    Remove laravel test code from HttpClient.

--- src/Http/GuzzleHttpClient.php
+++ src/Http/GuzzleHttpClient.php
@@ -22,8 +22,6 @@ class GuzzleHttpClient extends HttpClient
 
 	public function post($path, $data)
 	{
-		file_put_contents(base_path('esputnik.request.log'), json_encode($data, JSON_UNESCAPED_UNICODE));
-
 		$response = $this->guzzle->post($this->apiBaseUrl . $path, [
 			'auth' => [
 				$this->login,
